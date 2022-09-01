3107a21b4fc2bfc5648d7fe8f3ccce3c64cef8da
Nikita Masych <nikitamasich152@gmail.com>
2022-09-01 09:52:55 +0300

    Added .gitignore subscribed emails and ensurancy that directory for it's storage will be present

--- emails/email_data.go
+++ emails/email_data.go
@@ -6,9 +6,11 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strings"
 )
 
 func EnsureFileExists() {
+	EnsureDirectoryExist()
 	path := config.Get().StorageFile
 	_, err := os.Stat(path)
 	if os.IsNotExist(err) {
@@ -17,6 +19,18 @@ func EnsureFileExists() {
 			log.Fatal(err)
 		}
 		defer file.Close()
+	}
+}
+
+func EnsureDirectoryExist() {
+	path := config.Get().StorageFile
+	directory := strings.Split(path, "/")[0]
+	_, err := os.Stat(directory)
+	if os.IsNotExist(err) {
+		err = os.Mkdir(directory, 0700)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
